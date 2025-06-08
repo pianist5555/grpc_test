c9088fe3df7ce30d0d6748afc0742717d453a548
seono park <pianist5555@gmail.com>
2025-06-08 20:51:40 +0900

    feat: add timeout 5seconds

--- unary_streaming/client.py
+++ unary_streaming/client.py
@@ -5,7 +5,7 @@ import helloworld_pb2_grpc
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:# 채널 생성
         stub = helloworld_pb2_grpc.GreeterStub(channel)# 스텁 생성 (채널 정보 필요)
-        response = stub.SayHello(helloworld_pb2.HelloRequest(name="Alice"))# 요청 전송
+        response = stub.SayHello(helloworld_pb2.HelloRequest(name="Alice"),timeout=5)# 요청 전송
         print("Greeter client received: " + response.message)# 응답 출력
 
 if __name__ == "__main__":
